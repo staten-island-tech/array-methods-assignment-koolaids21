ccef9b8adf46a8dbd070a61c47892ab90062f97b
weihong <weihong2398@gmail.com>
2023-10-10 13:17:42 -0400

    zzz

---
+++ array.js
@@ -0,0 +1,43 @@
+const foods = [
+    {
+      name: "pizza",
+      flavor: "salty",
+      utensils: "hands",
+      nutreints: "fats",
+    },
+    {
+      name: "spicy chicken sandwich",
+      flavor: "spicy",
+      utensils: "hands",
+      nutreints: "protein",
+    },
+    {
+      name: "rice",
+      flavor: "non",
+      utensils: "spoon",
+      nutreints: "carbohydrate",
+    },
+    {
+      name: "fries",
+      flavor: "salty",
+      utensils: "hands",
+      nutrients: "potassium",
+    },
+  ];
+  
+  function printNames() {
+    foods.forEach((food) => {
+      console.log(food.name);
+    });
+  }
+  
+  function printProperties() {
+    foods.forEach((food) => {
+      console.log(food.flavor, food.utensils, food.nutrients);
+    });
+  }
+  
+  
+  console.log("food names");
+  printNames();
+  
