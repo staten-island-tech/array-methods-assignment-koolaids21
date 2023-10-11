3476f00aceac5a2115de0a3f5974fc66d5a01ef4
weihong <weihong2398@gmail.com>
2023-10-11 13:16:06 -0400

    zzzz

--- array.js
+++ array.js
@@ -1,45 +1,42 @@
 const foods = [
-    {
-      name: "pizza",
-      flavor: "salty",
-      utensils: "hands",
-      nutreints: "fats",
-    },
-    {
-      name: "spicy chicken sandwich",
-      flavor: "spicy",
-      utensils: "hands",
-      nutreints: "protein",
-    },
-    {
-      name: "rice",
-      flavor: "non",
-      utensils: "spoon",
-      nutreints: "carbohydrate",
-    },
-    {
-      name: "fries",
-      flavor: "salty",
-      utensils: "hands",
-      nutrients: "potassium",
-    },
-  ];
-  
-  function printNames() {
-    foods.forEach((food) => {
-      console.log(food.name);
-    });
-  }
-  
-  function printProperties() {
-    foods.forEach((food) => {
-      console.log(food.flavor, food.utensils, food.nutrients);
-    });
-  }
+  {
+    name: "pizza",
+    flavor: "salty",
+    utensils: "hands",
+    nutreints: "fats",
+  },
+  {
+    name: "spicy chicken sandwich",
+    flavor: "spicy",
+    utensils: "hands",
+    nutreints: "protein",
+  },
+  {
+    name: "rice",
+    flavor: "non",
+    utensils: "spoon",
+    nutreints: "carbohydrate",
+  },
+  {
+    name: "fries",
+    flavor: "salty",
+    utensils: "hands",
+    nutrients: "potassium",
+  },
+];
 
-  const result = foods.filter((food) => food.length > 6);
-  console.log(results);
-  
-  console.log("food names");
-  printNames();
-  
+function printNames() {
+  foods.forEach((food) => {
+    console.log(food.name);
+  });
+}
+
+function printProperties() {
+  foods.forEach((food) => {
+    console.log(food.flavor, food.utensils, food.nutrients);
+  });
+}
+
+
+console.log("food names");
+printNames();
